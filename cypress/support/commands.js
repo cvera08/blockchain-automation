76a25d688f698da3ab2ff3e430afe935f487395a
cvera08 <cvera08@gmail.com>
2023-01-05 18:54:44 -0300

    build: not to print application exceptions

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,4 +24,12 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-import '@testing-library/cypress/add-commands' //Support for cy.getBy*** selectors
+//Support for cy.getBy*** selectors
+import '@testing-library/cypress/add-commands'
+
+// Avoid uncaught:exception (application exceptions)
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false
+})
