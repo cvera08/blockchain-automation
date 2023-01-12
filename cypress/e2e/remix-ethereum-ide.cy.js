2ab5349ae4bb580be368842b795d8d5e8d9bcbd4
cvera08 <cvera08@gmail.com>
2023-01-12 18:47:42 -0300

    feat(new-atc): delete first contract - draft

--- cypress/e2e/remix-ethereum-ide.cy.js
+++ cypress/e2e/remix-ethereum-ide.cy.js
@@ -20,9 +20,22 @@ describe('remix ide spec', () => {
     sidePanel.deployRunTransactionsTitle().should('be.visible').and('have.text', 'Deploy & run transactions')
   })
 
-  it('first smart contract - wip', () => {
+  it('delete first contract', () => {
     fileExplorer().click()
     sidePanel.validateFileExplorerTitle()
+    cy.wait(4000) //I mandatorily need to wait here since there is a flash from default_workspace to localhost and come back again
+    cy.findByRole('button', {name: /default_workspace/i});
+    //cy.findByRole('button', {name: /localhost/i}).should("not.be.visible")
+    cy.get('ul[data-id="treeViewUltreeViewMenu"] span[title="contracts"]', {timeout: 10000}).click({force: true})
+    //const firstContract = cy.get(....) //TODO: take the name of the contract
+    cy.get('ul[data-id="treeViewUltreeViewcontracts"] li').first().rightclick({force: true})
+    cy.get('li[id="menuitemdelete"]').click()
+    //cy.wait(1000) //if we click so quick the real deletion is never performed
+    cy.get('[data-id="fileSystemModalDialogContainer-react"] > .modal-dialog .modal-ok').click({force: true})
+    //TODO: validate firstContract (name) is not visible anymore / does not exist
+  })
+
+  it('first smart contract - wip', () => {
     // TODO: finish this test
   })
 })
