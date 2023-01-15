aaa3ea8b29dbb91929a351d69a0f4c4c728a1d3f
cvera08 <cvera08@gmail.com>
2023-01-15 17:19:26 -0300

    feat(new-atc): add new contract

--- cypress/e2e/remix-ethereum-ide.cy.js
+++ cypress/e2e/remix-ethereum-ide.cy.js
@@ -13,6 +13,12 @@ describe('remix ide spec', () => {
     cy.log(".")
   })
 
+  /* Important: 
+  testIsolation: false (cypress.config.js)
+  This suite is running one test after the other considering the last stage of the previous one,
+  even when it is a well-known bad practice (it generates dependencies instead of atomic ATCs).
+  This is because the online remix IDE is slow in its start/refresh and even the application is error-prone in several launches. 
+  In this way, the whole execution is much faster than reloading each time. */
   it('deploy & run transactions', () => {
     sidePanel.validateFileExplorerTitle()
 
@@ -26,33 +32,29 @@ describe('remix ide spec', () => {
     cy.wait(4000) //I mandatorily need to wait here since there is a flash from default_workspace to localhost and come back again
     sidePanel.defaultWorkspaceDdl().should('be.visible')
     sidePanel.contractsFolder().click({ force: true })
-    sidePanel.firstContract().then($value => {
+    sidePanel.firstContractName().then($value => {
       cy.wrap($value.text()).as('firstContractName') //Grab the name of the current contract
     })
-    sidePanel.firstContract().rightclick({ force: true })
+    sidePanel.firstContractName().rightclick({ force: true })
     sidePanel.menuItemDelete().click()
     modalSelectors.deleteItemOK().click({ force: true })
     cy.get('@firstContractName').then((firstContract) => {
-      sidePanel.firstContract()
+      sidePanel.firstContractName()
         .invoke('text').should("not.eq", firstContract) //Validate first contract name is not visible anymore / does not exist 
     })
   })
 
-  it.only('add new smart contract', () => {
-    //fileExplorer().click()
-    //sidePanel.validateFileExplorerTitle()
-    cy.wait(10000)
-    sidePanel.contractsFolder().click({ force: true })
-    cy.get('ul[data-id="treeViewUltreeViewcontracts"] li').its('length').then((number) => {
+  it('add new smart contract', () => {
+    const contractName = `X_atc_${Cypress._.random(0, 1e6)}.sol`
+
+    sidePanel.contractList().its('length').then((number) => {
       cy.wrap(number).as('contractsAmount')
     })
-    cy.get('span[id="createNewFile"]').click().type('hello.sol{enter}')
-    //cy.get('ul[data-id="treeViewUltreeViewcontracts"] li:nth-of-type(4)').type('hello.sol{enter}')
+    sidePanel.createNewFile().click().type(`${contractName}{enter}`)
 
     cy.get('@contractsAmount').then((contractsAmount) => { //validate there is one new element in the contract list
-      cy.get('ul[data-id="treeViewUltreeViewcontracts"] li').its('length').should('be.equal', contractsAmount + 1)
+      sidePanel.contractList().its('length').should('eq', contractsAmount + 1)
     })
-
-    //TODO: validate the new contract name is present
+    sidePanel.contractListNames().contains(contractName) //validate the new contract name is present
   })
 })
