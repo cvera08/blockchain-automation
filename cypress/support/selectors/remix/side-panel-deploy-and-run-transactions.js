7cda1a64fd2ab23420d40aaf4b6bc2b0451cab66
cvera08 <cvera08@gmail.com>
2023-06-18 12:49:51 -0300

    refactor: remix -wip6

--- cypress/support/selectors/remix/side-panel-deploy-and-run-transactions.js
+++ cypress/support/selectors/remix/side-panel-deploy-and-run-transactions.js
@@ -28,3 +28,18 @@ export const saveCurrentNumberHotFudgeSauce = (currentValueName) => getUintText(
             .then(parseInt)
             .as(currentValueName)
     )
+
+/**
+ * since negatives numbers are not allowed: 
+ *      we need to increment until is greater than zero, otherwise 0-1=0 and expects won't work
+ * @returns 
+ */
+export const incrementMoreThanZero = () => cy.then(
+    function () {
+        if (this.originalNumberHotFudgeSauce === 0) { //increment
+            incrementDeployedContractBtn().click().wait(1000)
+
+            getDeployedContractBtn().click().wait(500)
+            saveCurrentNumberHotFudgeSauce('originalNumberHotFudgeSauce')
+        }
+    })
